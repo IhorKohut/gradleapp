6d89205b2546076c577808830496bac67b493caf
IhorKohut <ihor.kohut@ringteam.com>
2020-05-13 22:50:32 +0300

    change homework

--- src/test/java/com/example/tests/TestParameters.java
+++ src/test/java/com/example/tests/TestParameters.java
@@ -1,12 +1,28 @@
 package com.example.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+
+// This test should run from testng.xml
 
 public class TestParameters {
     @Parameters({"country", "capital"})
     @Test
     public void parameterTest(String country, String capital){
+        System.out.println("Test with Parameters");
+        System.out.println(capital + " is the capital of " + country);
+    }
+    @DataProvider(name = "Country")
+    public Object[][] createData1() {
+        return new Object[][] {
+                { "Ukraine", "Kiev"}
+        };
+    }
+
+    @Test(dataProvider = "Country")
+    public void verifyData1(String country, String capital) {
+        System.out.println("Test with data Provider");
         System.out.println(capital + " is the capital of " + country);
     }
 }
